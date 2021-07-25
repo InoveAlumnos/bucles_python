b353b12ead4060e7a77667e41582b4eb97290518
InoveAlumnos <64550682+InoveAlumnos@users.noreply.github.com>
2021-07-25 17:56:36 -0300

    Update ejercicio_5.py

--- ejercicios_practica/ejercicio_5.py
+++ ejercicios_practica/ejercicio_5.py
@@ -18,6 +18,7 @@
 
 inicio = int(input('Ingrese el primer número de la secuencia\n'))
 fin = int(input('Ingrese el último número de la secuencia\n'))
+sumatoria = 0  # Inicializo el contador en 0
 
 # for ... in range(....)
 
