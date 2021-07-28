f2763a87947b1312c48f62af3131c923af6bc852
InoveAlumnos <64550682+InoveAlumnos@users.noreply.github.com>
2021-07-27 21:11:04 -0300

    Update ejemplo_3.py

--- ejemplos_clase/ejemplo_3.py
+++ ejemplos_clase/ejemplo_3.py
@@ -15,16 +15,17 @@
 # Como vamos a "contar", el contador debe arrancar en "0"
 contador = 0
 
-print("Ingrese cualquier número entero mayor a cero")
-print("Ingrese número negativo para teriminar el programa")
-
 while True:
+    print("Ingrese cualquier número entero mayor o igual a cero")
+    print("Ingrese número negativo para teriminar el programa")
     numero = int(input())
 
     if numero >= 0:
         if(numero % 2) == 0:    # El número es par?
             contador += 1       # Incremento el contador
             print("Contador =", contador)
+        else:
+            print("Se ha ingresado un número impar, no se aumenta el contador")
     else:
         # Terminó el bucle
         print("Número ingresado negativo, termina el bucle")
